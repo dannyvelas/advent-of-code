44657bd6bdc114869fe4e6b7e0327be8752232d7
Daniel Velasquez <danivelas4@gmail.com>
2024-01-13 22:23:13 -0500

    2022-clojure/src/day01/part1/core.clj: made more like apl sol

--- 2022-clojure/src/day01/part1/core.clj
+++ 2022-clojure/src/day01/part1/core.clj
@@ -3,17 +3,13 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn add-up-vec [v]
-  (reduce + v))
-
-(defn int-vec [v]
-  (map #(Integer/parseInt %) v))
+(defn to-int [v]
+  (if (= v "") 0
+      (Integer/parseInt v)))
 
 (defn -main [& _]
-  (let [file-content (slurp (io/resource "01-calories.txt"))
-        calorie-groups (str/split file-content #"\n\n")
-        calorie-matrix (map #(str/split % #"\n") calorie-groups)
-        calorie-matrix-num (map int-vec calorie-matrix)
-        added-up-calories (map add-up-vec calorie-matrix-num)
-        max-calories (apply max added-up-calories)]
-    (println max-calories)))
+  (->> (str/split (slurp (io/resource "01-calories.txt")) #"\n")
+       (partition-by (partial = ""))
+       (map (partial map to-int))
+       (map (partial reduce +))
+       (apply max)))
