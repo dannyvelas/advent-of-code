6bc99a04e5ef686fb08bdd253f33fdbf5d4eb551
Daniel Velasquez <danivelas4@gmail.com>
2024-12-07 18:57:29 -0500

    A 2024-bash-jq/day02/02.sh

---
+++ 2024-bash-jq/day02/02.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#'
+#[2 2 2 2 2 1]
+#[2 2 1]
+#[1 2 1] => [1, -1]
+#'
+
+cat input.txt | jq -Rs '
+  def is_safe:
+    . as $arr
+    | [range(1; length) | $arr[.] - $arr[.-1]] as $diffs
+    | ([$diffs[]|select(. > 0)]|length) as $amt_pos
+    | ([$diffs[]|select(. < 0)]|length) as $amt_neg
+    | (if $amt_pos >= $amt_neg then "pos" else "neg" end) as $state
+    | [$diffs[] | (. != 0 and abs <= 3 and ((. > 0 and $state == "pos") or (. < 0 and $state == "neg")))] as $level_results
+    | (([$level_results[] | select(.|not)]|length) <= 1)
+  ;
+  .
+  | split("\n")[:-1]
+  | [ .[] | split(" ") | [.[] | tonumber] ]
+  | reduce .[] as $report(0; if ($report | is_safe) then .+1 else . end)
+'
