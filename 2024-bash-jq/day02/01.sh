5b836ba1d7be6d5252896c6d1bdd98593d0f56fa
Daniel Velasquez <danivelas4@gmail.com>
2024-12-07 18:22:26 -0500

    M 2024-bash-jq/day02/01.sh

--- 2024-bash-jq/day02/01.sh
+++ 2024-bash-jq/day02/01.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
 cat input.txt | jq -Rs '
-  def is_safe:
-    (if .[0] < .[1] then "increasing" else "decreasing" end) as $state
-    | . as $arr
-    | [range(1; length) | $arr[.] - $arr[.-1]] 
-    | if ([.[] | select(. == 0 or abs > 3)] | length > 0) then
-        false
-      elif ([.[] | select(. < 0)] | length > 0) and ([.[] | select(. > 0)] | length > 0) then
-        false
-      else
-        true
-      end
-  ;
-
   .
   | split("\n")[:-1]
   | [ .[] | split(" ") | [.[] | tonumber] ]
   | reduce .[] as $report(
       0;
-      if ($report | is_safe) then . + 1 else . end
+      (if $report[0] < $report[1] then "increasing" else "decreasing" end) as $state
+      | [range(1; $report | length) | $report[.] - $report[.-1]] as $diffs
+      | if ([$diffs[] | select(. == 0 or abs > 3)] | length > 0) then
+          .
+        elif ([$diffs[] | select(. < 0)] | length > 0) and ([$diffs[] | select(. > 0)] | length > 0) then
+          .
+        else
+          . + 1
+        end
     )
 '
