03b6b925fcc8c968e5364abaace1ed77c1572ece
Daniel Velasquez <danivelas4@gmail.com>
2024-12-07 17:13:39 -0500

    A 2024-bash-jq/day01/02.sh

---
+++ 2024-bash-jq/day01/02.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+cat 01-input.txt | jq -Rs '
+  split("\n")[:-1]
+  | [ .[] | split("   ") ]
+  | (reduce .[] as $item (
+      {};
+      if .[$item[1]] == null then
+        .[$item[1]] = 1
+      else
+        .[$item[1]] = .[$item[1]] + 1
+      end
+    )) as $secondFreqMap
+  | reduce .[] as $item (
+      0;
+      if $secondFreqMap[$item[0]] != null then
+        . + (($item[0]|tonumber) * $secondFreqMap[$item[0]])
+      else
+        .
+      end
+    )
+'
