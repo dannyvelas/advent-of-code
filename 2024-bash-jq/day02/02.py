0cd5795d0ad633f4fe067f03305c6d4d020e9598
Daniel Velasquez <danivelas4@gmail.com>
2024-12-08 10:29:36 -0500

    M 2024-bash-jq/day02/02.py

--- 2024-bash-jq/day02/02.py
+++ 2024-bash-jq/day02/02.py
@@ -8,29 +8,24 @@ def main():
     for report in reports:
         if is_safe(report):
             amt_safe += 1
-    return amt_safe
+    print(amt_safe)
 
 def is_safe(report) -> bool:
-    state = get_state(report)
-
     stack = []
-    seen_error = False
+    seen_err = False
     for level in report:
-        if not stack or (is_good_step(stack[-1], level, state) and abs(level - stack[-1]) <= 3:
-            stack.append(level)
-        else:
-            pass
+        if stack and not is_good_step(stack[-1], level, state):
+            if seen_err: return False
+            seen_err = True
+            continue
+        while stack and abs(level - stack[-1]) > 3:
+            if seen_err: return False
+            seen_err = True
+            stack.pop()
+        stack.append(level)
+    return True
             
 def is_good_step(prev, level, state) -> bool:
     return (level > prev and state=="inc") or (level < prev and state=="dec")
-
-def get_state(report):
-    amt_inc, amt_dec = 0, 0
-    for i in range(1, len(report)):
-        if report[i] >= report[i-1]:
-            amt_inc += 1
-        else:
-            amt_dec += 1
-    return "inc" if amt_inc >= amt_dec else "dec"
 
 main()
