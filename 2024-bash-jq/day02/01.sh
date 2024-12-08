c519ce76c88ff33332af35f83a0396c74c796eeb
Daniel Velasquez <danivelas4@gmail.com>
2024-12-08 11:51:03 -0500

    M 2024-bash-jq/day02/01.sh

--- 2024-bash-jq/day02/01.sh
+++ 2024-bash-jq/day02/01.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 cat input.txt | jq -Rs '
+  def is_safe:
+    . as $arr
+    | [range(1; length) | $arr[.] - $arr[.-1]] as $diffs
+    | [range(1; $diffs|length) | [$diffs[.-1], $diffs[.]]] as $grouped_diffs
+    | ([$diffs[] | select(. == 0 or abs > 3)] | length == 0) and
+      ([$grouped_diffs[] | select((.[0] > 0) != (.[1] > 0))] | length == 0)
+  ;
   .
   | split("\n")[:-1]
   | [ .[] | split(" ") | [.[] | tonumber] ]
-  | reduce .[] as $report(
-      0;
-      [range(1; $report | length) | $report[.] - $report[.-1]] as $diffs
-      | if ([$diffs[] | select(. == 0 or abs > 3)] | length > 0) then
-          .
-        elif ([$diffs[] | select(. < 0)] | length > 0) and ([$diffs[] | select(. > 0)] | length > 0) then
-          .
-        else
-          . + 1
-        end
-    )
+  | reduce .[] as $report(0; if ($report|is_safe) then .+1 else . end)
 '
