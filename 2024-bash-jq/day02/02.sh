fbf18bd10e10929a466eb483a8adb40e5ee2f174
Daniel Velasquez <danivelas4@gmail.com>
2024-12-08 12:02:01 -0500

    M 2024-bash-jq/day02/02.sh

--- 2024-bash-jq/day02/02.sh
+++ 2024-bash-jq/day02/02.sh
@@ -6,25 +6,22 @@
 #[1 2 1] => [1, -1]
 #'
 
-cat input.txt | jq -Rs '
+cat input.txt | jq -Rsc '
   def is_safe:
     . as $arr
     | [range(1; length) | $arr[.] - $arr[.-1]] as $diffs
-    | ([$diffs[]|select(. > 0)]|length) as $amt_pos
-    | ([$diffs[]|select(. < 0)]|length) as $amt_neg
-    | (if $amt_pos >= $amt_neg then "pos" else "neg" end) as $state
-    | [$diffs[] | select((. > 0 and $state == "pos") or (. < 0 and $state == "neg"))] as $goodsteps
-    | if (($goodsteps|length) < (($diffs|length)-1)) then
-        false
-      else
-        [range(1; $goodsteps|length) | $goodsteps[.] - $goodsteps[.-1]]
-        | ([.[] | select(abs > 3)] | length>0)
-      end
+    | [range(1; $diffs|length) | [$diffs[.-1], $diffs[.]]] as $grouped_diffs
+    | ([$diffs[] | select(. == 0 or abs > 3)] | length == 0) and
+      ([$grouped_diffs[] | select((.[0] > 0) != (.[1] > 0))] | length == 0)
   ;
-  .
-  | split("\n")[:-1]
+  
+  def is_safe_dampened:
+    . as $arr
+    | [ range(length) | ($arr[:.] + $arr[.+1:])]
+    | [ .[] | is_safe]
+    | any
+  ;
+  split("\n")[:-1]
   | [ .[] | split(" ") | [.[] | tonumber] ]
-  | .[]
-  | is_safe
-  #| reduce .[] as $report(0; if ($report | is_safe) then .+1 else . end)
+  | reduce .[] as $report(0; if ($report | is_safe_dampened) then .+1 else . end)
 '
