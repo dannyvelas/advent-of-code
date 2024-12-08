c8b59221ea0917ee5b7e996224b748d75f899102
Daniel Velasquez <danivelas4@gmail.com>
2024-12-08 10:49:53 -0500

    writing errichto solution M 2024-bash-jq/day02/02.py

--- 2024-bash-jq/day02/02.py
+++ 2024-bash-jq/day02/02.py
@@ -1,4 +1,20 @@
 import json
+from typing import List
+
+def is_ok(report: List[int]) -> bool:
+    ok = True
+    only_inc = True
+    only_dec = True
+    for i in range(len(report)-1):
+        diff = report[i+1]-report[i]
+        if diff > 0:
+            only_dec = False
+        elif diff < 0:
+            only_inc = False
+        if not (1 <= abs(diff) <= 3):
+            ok = False
+            break
+    return ok and (only_inc or only_dec)
 
 def main():
     reports_input = input()
@@ -6,26 +22,30 @@ def main():
 
     amt_safe = 0
     for report in reports:
-        if is_safe(report):
-            amt_safe += 1
-    print(amt_safe)
+        any_ok = False
+        def consider(i: int):
+            nonlocal any_ok
+            b = report[:i] + report[i+1:]
+            if is_ok(b):
+                any_ok = True
 
-def is_safe(report) -> bool:
-    stack = []
-    seen_err = False
-    for level in report:
-        if stack and not is_good_step(stack[-1], level, state):
-            if seen_err: return False
-            seen_err = True
-            continue
-        while stack and abs(level - stack[-1]) > 3:
-            if seen_err: return False
-            seen_err = True
-            stack.pop()
-        stack.append(level)
-    return True
-            
-def is_good_step(prev, level, state) -> bool:
-    return (level > prev and state=="inc") or (level < prev and state=="dec")
+        consider(0)
+        for i in range(len(report)-1):
+            diff = report[i+1]-report[i]
+            if abs(diff) < 1 or abs(diff) > 3:
+                consider(i)
+                consider(i+1)
+                break
+            elif i + 2 < len(report):
+                diff_2 = report[i+2] - report[i+1]
+                if (diff > 0) != (diff_2 > 0):
+                    consider(i)
+                    consider(i+1)
+                    consider(i+2)
+                    break
+        if any_ok:
+            amt_safe += 1
+
+    print(amt_safe)
 
 main()
