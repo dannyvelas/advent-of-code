cdd322bdb97aae8787474a15a7cb5ea106f50645
Daniel Velasquez <danivelas4@gmail.com>
2024-12-08 12:06:55 -0500

    M 2024-bash-jq/day02/02.sh

--- 2024-bash-jq/day02/02.sh
+++ 2024-bash-jq/day02/02.sh
@@ -6,7 +6,7 @@
 #[1 2 1] => [1, -1]
 #'
 
-cat input.txt | jq -Rsc '
+cat input.txt | jq -Rn '
   def is_safe:
     . as $arr
     | [range(1; length) | $arr[.] - $arr[.-1]] as $diffs
@@ -21,7 +21,13 @@ cat input.txt | jq -Rsc '
     | [ .[] | is_safe]
     | any
   ;
-  split("\n")[:-1]
-  | [ .[] | split(" ") | [.[] | tonumber] ]
-  | reduce .[] as $report(0; if ($report | is_safe_dampened) then .+1 else . end)
+  
+  reduce inputs as $report(
+    0; 
+    if ($report | split(" ") | [.[] | tonumber] | is_safe_dampened) then
+      .+1
+    else
+      .
+    end
+  )
 '
