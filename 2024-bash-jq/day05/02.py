f6db037d0b973b63c663f027374b3f698937df00
Daniel Velasquez <danivelas4@gmail.com>
2024-12-13 10:18:06 -0500

    Revert "M 2024-bash-jq/day05/02.py"
    This reverts commit 1a90921ba1e8535842ed150e5c522167db16efd4.

--- 2024-bash-jq/day05/02.py
+++ 2024-bash-jq/day05/02.py
@@ -1,53 +1,50 @@
 import sys
 from collections import defaultdict
-from typing import List, Set, DefaultDict
+from typing import List, Dict, Set
 
 graph = defaultdict(set)
-
-def topo(node: str, present: Set[str], result: List[str], _in: DefaultDict[str, int]):
-    def dfs(node: str):
-        result.append(node)
-
-        for neighbor in graph[node]:
-            if neighbor in present:
-                _in[neighbor] -= 1
-                if _in[neighbor] == 0:
-                    dfs(neighbor)
-
-    dfs(node)
-    return 
-
 for line in sys.stdin:
     if line == '\n': break
     needed_before, needed_after = line[:-1].split('|')
-    graph[needed_before].add(needed_after)
+    graph[needed_after].add(needed_before)
+
+
+def sort(present: Set[str]) -> List[str]:
+    visited = set()
+    result = []
+
+    def dfs(node: str):
+        if node in visited: return
+        visited.add(node)
+
+        for neighbor in graph[node]:
+            if neighbor in present:
+                dfs(neighbor)
+
+        result.append(node)
+
+    for node in graph:
+        dfs(node)
+
+    return result
 
 lines = []
 for line in sys.stdin:
     lines.append(line[:-1].split(','))
 
 ans = 0
-i = 0
 for line in lines:
     blacklist = set()
     was_blacklisted = False
     present = set()
-    _in = defaultdict(int)
     for num in line:
-        for edge in graph[num]:
-            _in[edge] += 1
         if num in blacklist:
             was_blacklisted = True
         blacklist.update(graph[num])
         present.add(num)
+    result = []
     if was_blacklisted:
-        result = []
-        for node in line:
-            if _in[node] == 0:
-                topo(node, present, result, _in)
-        print(i)
-        print(result)
+        result = sort(present)
         ans += int(result[len(result) // 2])
-    i += 1
 
 print(ans)
