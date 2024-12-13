1a90921ba1e8535842ed150e5c522167db16efd4
Daniel Velasquez <danivelas4@gmail.com>
2024-12-13 10:16:19 -0500

    M 2024-bash-jq/day05/02.py

--- 2024-bash-jq/day05/02.py
+++ 2024-bash-jq/day05/02.py
@@ -1,50 +1,53 @@
 import sys
 from collections import defaultdict
-from typing import List, Dict, Set
+from typing import List, Set, DefaultDict
 
 graph = defaultdict(set)
-for line in sys.stdin:
-    if line == '\n': break
-    needed_before, needed_after = line[:-1].split('|')
-    graph[needed_after].add(needed_before)
 
-
-def sort(present: Set[str]) -> List[str]:
-    visited = set()
-    result = []
-
+def topo(node: str, present: Set[str], result: List[str], _in: DefaultDict[str, int]):
     def dfs(node: str):
-        if node in visited: return
-        visited.add(node)
+        result.append(node)
 
         for neighbor in graph[node]:
             if neighbor in present:
-                dfs(neighbor)
+                _in[neighbor] -= 1
+                if _in[neighbor] == 0:
+                    dfs(neighbor)
 
-        result.append(node)
+    dfs(node)
+    return 
 
-    for node in graph:
-        dfs(node)
-
-    return result
+for line in sys.stdin:
+    if line == '\n': break
+    needed_before, needed_after = line[:-1].split('|')
+    graph[needed_before].add(needed_after)
 
 lines = []
 for line in sys.stdin:
     lines.append(line[:-1].split(','))
 
 ans = 0
+i = 0
 for line in lines:
     blacklist = set()
     was_blacklisted = False
     present = set()
+    _in = defaultdict(int)
     for num in line:
+        for edge in graph[num]:
+            _in[edge] += 1
         if num in blacklist:
             was_blacklisted = True
         blacklist.update(graph[num])
         present.add(num)
-    result = []
     if was_blacklisted:
-        result = sort(present)
+        result = []
+        for node in line:
+            if _in[node] == 0:
+                topo(node, present, result, _in)
+        print(i)
+        print(result)
         ans += int(result[len(result) // 2])
+    i += 1
 
 print(ans)
