abfb85906f06251627a3c7485b95a5e8f4d1d2dc
Daniel Velasquez <danivelas4@gmail.com>
2024-12-14 19:52:38 -0500

    A 2024-bash-jq/day08/01.py

---
+++ 2024-bash-jq/day08/01.py
@@ -0,0 +1,42 @@
+import sys
+
+def main():
+    lines = []
+    for line in sys.stdin:
+        lines.append(line[:-1])
+    
+    antennas = dict()
+    n, m = len(lines), len(lines[0])
+    for i in range(len(lines)):
+        for j in range(len(lines[i])):
+            c = lines[i][j]
+            if c == '.': continue
+            if c not in antennas:
+                antennas[c] = []
+            antennas[c].append((i, j))
+
+    def in_bounds(i: int, j: int) -> int:
+        return i >= 0 and i < n and j >= 0 and j < m
+
+    antinodes = set()
+    for group in antennas.values():
+        for i in range(len(group)):
+            start_i, start_j = group[i]
+            for n_i, n_j in group[i+1:]:
+                # calculate x and y axis diff
+                dx = n_i - start_i # 1
+                dy = n_j - start_j # -3
+
+                # calc antinodes
+                (a1i, a1j) = start_i - dx, start_j - dy
+                (a2i, a2j) = n_i + dx, n_j + dy
+
+                if in_bounds(a1i, a1j):
+                    antinodes.add((a1i, a1j))
+                if in_bounds(a2i, a2j):
+                    antinodes.add((a2i, a2j))
+
+    print(len(antinodes))
+
+
+main()
