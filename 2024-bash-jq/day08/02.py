9846dfd7a717d08746aad0df45839bce0f244222
Daniel Velasquez <danivelas4@gmail.com>
2024-12-14 20:09:24 -0500

    M 2024-bash-jq/day08/01.py A 2024-bash-jq/day08/02.py

--- 2024-bash-jq/day08/02.py
+++ 2024-bash-jq/day08/02.py
@@ -19,23 +19,33 @@ def main():
         return i >= 0 and i < n and j >= 0 and j < m
 
     antinodes = set()
-    for group in antennas.values():
-        for i in range(len(group)):
-            start_i, start_j = group[i]
-            for n_i, n_j in group[i+1:]:
+    for key in antennas:
+        for i in range(len(antennas[key])):
+            start_i, start_j = antennas[key][i]
+            #print(f'{key} {start_i} {start_j}')
+            if len(antennas[key]) > 1:
+                antinodes.add((start_i, start_j))
+            for n_i, n_j in antennas[key][i+1:]:
+                #print(f'\t{n_i},{n_j}')
                 # calculate x and y axis diff
                 dx = n_i - start_i # 1
                 dy = n_j - start_j # -3
 
-                # calc antinodes
+                # calc antinodes line
                 (a1i, a1j) = start_i - dx, start_j - dy
-                (a2i, a2j) = n_i + dx, n_j + dy
-
-                if in_bounds(a1i, a1j):
+                while in_bounds(a1i, a1j):
+                    #print(f'\t\t{a1i},{a1j}')
                     antinodes.add((a1i, a1j))
-                if in_bounds(a2i, a2j):
-                    antinodes.add((a2i, a2j))
+                    (a1i, a1j) = (a1i - dx, a1j - dy)
 
+                (a2i, a2j) = n_i + dx, n_j + dy
+                while in_bounds(a2i, a2j):
+                    #print(f'\t\t{a2i},{a2j}')
+                    antinodes.add((a2i, a2j))
+                    (a2i, a2j) = (a2i + dx, a2j + dy)
+
+
+    #print(antinodes)
     print(len(antinodes))
 
 
