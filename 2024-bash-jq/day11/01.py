dea4c9922d203a075453816acccd5d21fb62e64e
Daniel Velasquez <daniel.velasquez0@walmart.com>
2024-12-16 13:19:43 -0500

    M 2024-bash-jq/day11/01.py

--- 2024-bash-jq/day11/01.py
+++ 2024-bash-jq/day11/01.py
@@ -2,10 +2,8 @@ import sys
 from typing import Tuple
 import math
 
-def split_num(num: int) -> Tuple[int, int]:
-    divisor = 10
-    while num // divisor > divisor:
-        divisor *= 10
+def split_num(num_digits: int, num: int) -> Tuple[int, int]:
+    divisor = 10 ** (num_digits // 2)
     return num // divisor, num % divisor
 
 def main():
@@ -16,11 +14,13 @@ def main():
         for num in nums:
             if num == 0:
                 new_nums.append(1)
-            elif (math.floor(math.log10(num))+1) % 2 == 0:
-                l, r = split_num(num)
-                new_nums.extend([l, r])
             else:
-                new_nums.append(num * 2024)
+                num_digits = math.floor(math.log10(num))+1
+                if num_digits % 2 == 0:
+                    l, r = split_num(num_digits, num)
+                    new_nums.extend([l, r])
+                else:
+                    new_nums.append(num * 2024)
             nums = new_nums
 
     print(len(nums))
